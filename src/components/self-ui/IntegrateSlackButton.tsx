9a71f6fe21232f394f5a6d53f87b90f56e0cf7b1
shailya <shailya615141@gmail.com>
2024-07-19 18:09:40 +0530

    SLack button changes

--- src/components/self-ui/IntegrateSlackButton.tsx
+++ src/components/self-ui/IntegrateSlackButton.tsx
@@ -1,22 +1,22 @@
-'use client'
-import Link from 'next/link';
-import React from 'react';
+// 'use client'
+// import Link from 'next/link';
+// import React from 'react';
 
-const IntegrateSlackButton = () => {
-    return (
-      <div  className="text-foreground flex flex-1 justify-end pr-6">
-      <Link href={`https://slack.com/oauth/v2/authorize?client_id=7407880214262.7399981384887&scope=chat:write,channels:read,channels:join,calls:read,chat:write.public,mpim:read,im:write,groups:read&redirect_uri=https://0fb7-103-139-56-151.ngrok-free.app/api/slack/callback`}>
-        <img
-          alt="Add to Slack"
-          height="40"
-          width="139"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </Link>
-      </div>
-    );
-  };
+// const IntegrateSlackButton = () => {
+//     return (
+//       <div  className="text-foreground flex flex-1 justify-end pr-6">
+//       <Link href={`https://slack.com/oauth/v2/authorize?client_id=7407880214262.7399981384887&scope=chat:write,channels:read,channels:join,calls:read,chat:write.public,mpim:read,im:write,groups:read&redirect_uri=https://0fb7-103-139-56-151.ngrok-free.app/api/slack/callback`}>
+//         <img
+//           alt="Add to Slack"
+//           height="40"
+//           width="139"
+//           src="https://platform.slack-edge.com/img/add_to_slack.png"
+//           srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+//         />
+//       </Link>
+//       </div>
+//     );
+//   };
   
-  export default IntegrateSlackButton;
+//   export default IntegrateSlackButton;
   
